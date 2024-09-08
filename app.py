ef178b1849fa721316e366e38fce16d139665d83
Charlxyz <deleted.user.0065@gmail.com>
2024-09-08 22:45:25 +0200

    ...

--- app.py
+++ app.py
@@ -7,13 +7,10 @@ import os, uuid, base64, pytz, psutil, time, random, ssl, string, pyotp, logging
 # Créez l'application Flask
 app = Flask(__name__, template_folder='./flaskr/templates', static_folder='./flaskr/static')
 
-@app.before_first_request
-def setup_db():
-    CreateTable()
-
 app.route('/')
 def index():
     return render_template('principale.html')
 
 if __name__ == '__main__':
+    CreateTable()
     app.run(debug=True, host='0.0.0.0', port=5000)
