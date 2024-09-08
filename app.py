7bd2538a377db52176460a8bfa71aa7d3ab7a6b2
Charlxyz <deleted.user.0065@gmail.com>
2024-09-08 22:28:46 +0200

    rangement

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, jsonify, redirect, session, url_for, flash, abort
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from db import CreateTable
+import os, uuid, base64, pytz, psutil, time, random, ssl, string, pyotp, logging
+
+# Créez l'application Flask
+# app = Flask(__name__, template_folder='./flaskr/templates', static_folder='./flaskr/static')
+app = Flask(__name__)
+
+
+@app.before_first_request
+def setup_db():
+    CreateTable()
+
+app.route('/')
+def index():
+    return render_template('principale.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ db.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+def CreateUser():
+    con = sqlite3.connect("DataBase.db")
+    cur = con.cursor()
+    
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS User (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL,
+            username TEXT NOT NULL UNIQUE,
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            is_admin BOOLEAN DEFAULT 0
+        )
+    """)
+    
+    con.commit()
+    con.close()
+
+def CreateTable():
+    CreateUser()
